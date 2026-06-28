c116c1f2e1fe6941a96d7c70dc8ad4aca6fdf5d4
AverageJoe <157324509+Averagejoe-git@users.noreply.github.com>
2026-06-28 19:46:26 -0400

    Update hyprland.lua

--- hyprland.lua
+++ hyprland.lua
@@ -8,16 +8,3 @@ require("hyprland.rules")
 require("hyprland.execs")
 require("hyprland.animations")
 require("hyprland.env")
-
-
-
-
---[[
-	Just off of reference, this is how you inlcude custom configurations properly
-
- 	if is_file_exists(HOME .. "~/.config/hypr/custom/[section].lua") then
-		require("custom.[section]")
- 	end
-
- 	Section here means env/general/execs/rules etc. 
-  ]]
